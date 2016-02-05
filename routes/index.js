92eb6d22c8cc86c4eabddb0335b6e541351a0b0d
Ketil Aasarød <ketil.aasarod@ntnu.no>
2016-02-05 12:47:42 +0100

    initial

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var request = require('request');
 var express = require('express');
 var router = express.Router();
+var settings = require('../settings');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
     request('http://92.62.44.145/sales-service-rest/api/v1.0/ptas/BRA/productGroups', {
         headers: {
-            'X-Fara-ApiKey': 'c052fa4e-3294-4d01-a7a5-8116db925c43'
+            'X-Fara-ApiKey': settings.apiKey
         },
         qs: {
             productMedium: 4,
@@ -18,14 +19,19 @@ router.get('/', function (req, res, next) {
         },
         json: true
     }, function (err, response, body) {
-        if (err || response.statusCode !== 200) {
-            console.log('Failed!');
+        if (err) {
+            console.dir(err);
             return next(err);
+        }
+
+        if (response.statusCode !== 200) {
+            console.dir(response);
+            return next();
         }
 
         console.log(JSON.stringify(body, null, 2));
 
-        res.render('index', {title: 'Express', products: body});
+        res.render('index', {title: 'FARA product prices', products: body});
     });
 });
 
